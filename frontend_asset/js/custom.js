bb936d55074c651bf228f3d04c9f76600b186697
Md. Jafrul Hasan Rasel <jafrulras@gmail.com>
2022-02-12 18:27:18 +0600

    footer part done

--- frontend_asset/js/custom.js
+++ frontend_asset/js/custom.js
@@ -1,5 +1,25 @@
 $(function() {
 
+
+     // Testimonials_slider
+    $('.owl-carousel.Our_Lovely_Client_slider_item').owlCarousel({
+        loop:true,
+        margin:30,
+        autuplay: true,
+        center: true,
+        nav:false,
+        dots:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            }
+        }
+    });
+
+    // Our Service
     $('.owl-carousel').owlCarousel({
         loop:true,
         margin:30,
@@ -16,7 +36,11 @@ $(function() {
                 items:3
             }
         }
-    })
+    });
+
+
+
+   
 
 
 
