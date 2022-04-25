b2e660b4d6a7b6f1fb4cf772d44f3fe32ce9cfdc
Md. Jafrul Hasan Rasel <73773576+jhrasel@users.noreply.github.com>
2022-04-25 11:12:43 +0600

    change service part and clients part

--- frontend_asset/js/custom.js
+++ frontend_asset/js/custom.js
@@ -1,124 +1,76 @@
 $(function() {
 
-    // Dropdown Menu
-    $('.dropdown').hover(function() {
-            $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(300);
-        },
-        function()
-        {
-            $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(300);
-        }
-   );
-
-    //banner
-    $('.owl-carousel.banner_slider').owlCarousel({
-        loop:true,
-        margin:0,
-        autoplay: true,
-        autoPlaySpeed: 1000,
-        autoPlayTimeout: 1000,
-        center: true,
-        nav:false,
-        dots: true,
+    // case_slider
+    $('.owl-carousel.case_slider').owlCarousel({
+        // loop:true,
+        margin:30,
+        autoPlay: true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        responsiveClass:true,
         responsive:{
             0:{
-                items:1
+                items:1,
+                nav:true
             },
             600:{
-                items:1
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:true,
+                loop:false
+            }
+        }
+    })
+
+
+    // done_project_slider
+    $('.done_project_slider').owlCarousel({
+        loop:true,
+        margin:30,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        center: true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:3,
+            },
+            1000:{
+                items:4,
+                nav:true,
             }
         }
     });
 
-
-
-    //Category
-    $('.owl-carousel.category_slier_item').owlCarousel({
+    // our_clients_slider
+    $('.owl-carousel.our_clients_slider_part').owlCarousel({
         loop:true,
-        margin:0,
-        autuplay: true,
-        center: false,
-        nav:true,
-        dots: false,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:6
-            },
-            1000:{
-                items:8
-            },
-            1800:{
-                items:10,
-                margin:5,
-            },
-        }
-    });
-
-
-    //free_delivar_item
-    $('.owl-carousel.free_delivar_slider').owlCarousel({
-        loop:true,
-        margin:10,
-        autuplay: true,
-        center: true,
+        margin:30,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        responsiveClass:true,
         nav:false,
-        dots: false,
         responsive:{
             0:{
                 items:1
             },
             600:{
-                items:3
+                items:2
             },
             1000:{
                 items:3
-            },
-            1280:{
-                items:5
             }
         }
-    });
-
-
-    // Sticky Menu
-    $(window).scroll(function(){
-        if($(this).scrollTop() > 100){
-            $('.navbar').addClass('sticky')
-        } else{
-            $('.navbar').removeClass('sticky')
-        }
-    });
-
-    // BACK TO TOP
-
-    let toTopBtn = document.getElementById('back-top__content--desk');
-
-    toTopBtn.addEventListener('click', function(e) {
-    e.preventDefault();
-    window.scrollTo({top: 0, behavior: "smooth"});
-    });
-
-    //hide/show button on scroll up/down
-    let scrollPos = 0;
-
-    window.addEventListener('scroll', function(){
-
-    // detects new state and compares it with the new one
-    if ((document.body.getBoundingClientRect()).top > scrollPos) {
-        console.log('scrolling down')
-
-        document.getElementById('back-top').classList.remove('active');
-
-    } else {
-            document.getElementById('back-top').classList.add('active');
-        console.log('scrolling up')
-    }
-    // saves the new position for iteration.
-    scrollPos = (document.body.getBoundingClientRect()).top;
-    });
+    })
 
 
 
